32978a8f614a3cf0e2095514c8f4b5d58a20a29b
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-28 16:40:05 -0700

    commit tree a8b26a351a6d78916c537b9acd4f9f46326e741c

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let array = string.split(' ')
+    let specialWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newArray = array.map(function(element){
+      if (element === array[0] || !specialWords.find(word => word === element)){
+        return Formatter.capitalize(element)
+      }else{ return element }
+    })
+
+    return newArray.join(' ')
+  }
 }
